f98424a4cc884283aaba3408752c9c50a8c1ea33
CometLim <bluecomet0716@gmail.com>
2024-01-23 14:04:49 +0900

    Class Pratics 1

---
+++ class1.py
@@ -0,0 +1,19 @@
+# class1.py
+# 1) 클래스를 정의
+
+class Person:
+    #초기화 메서드
+    def __init__(self):
+        #인스턴스 멤버변수
+        self.name = "default name"
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+#2) 인스턴스 생성
+p1 = Person()
+p2 = Person()
+p1.name = "LHJ"
+
+#3) 메서드 호출
+p1.print()
+p2.print()
